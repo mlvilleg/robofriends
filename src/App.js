d3b8fb7994384905d49d750dcd8d2ac4fdbc63c5
Marc Villegas <mlvillegas@ucdavis.edu>
2023-04-30 17:46:46 -0700

    Update

--- src/App.js
+++ src/App.js
@@ -9,11 +9,22 @@ class App extends React.Component {
     constructor(){
         super();
         this.state = {
-            robots: robots,
+            robots: [],
             searchfield: ''        
             
         }
     }
+
+    componentDidMount(){
+        fetch('https://jsonplaceholder.typicode.com/users')
+            .then(response => response.json()
+            ).then(users => this.setState({robots:robots}))
+
+
+       
+    }
+
+
 
     onSearchChange = (event) => {
         this.setState({"searchfield": event.target.value})
